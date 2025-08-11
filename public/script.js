02b82bcb5f148e0e9f8b6a387474c133f25b71f7
DavidHermosi <b2029dibhermoso@pshs.edu.ph>
2025-08-11 08:50:18 +0800

    enter??

--- public/script.js
+++ public/script.js
@@ -1,2 +1,14 @@
-// no script tag needed here
-alert("Hi");
+let age=prompt("Enter your age:", 14);
+let maxAge =prompt("You will live until the ripe old age of : ", "100");
+let numPerDay = prompt("Number of Snacks Daily: ", 2);
+let sure = confirm("Sure of your numbers?");
+if (sure) {
+    let totalRequired = (Number(numPerDay) * 365) * (Number(maxAge) - Number(age));
+
+    document.write("You will need " + totalReqired + "to last you until the ripe old age of " + maxAge + "<br>");
+    alert("You will need " + totalRequired + " to last you until the ripe old age of " + maxAge);
+    console.log("You will need " + totalRequired + " to last you until the ripe old age of " + maxAge);
+}
+else {
+    alert("Please try again.");
+}
